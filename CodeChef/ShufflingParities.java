644b475a4c2f4753c419f96d1ff64d6e3ef22553
rakhi2207 <jharakhi1211@gmail.com>
2021-09-10 23:56:53 +0530

    Java Practice

--- CodeChef/ShufflingParities.java
+++ CodeChef/ShufflingParities.java
@@ -1,5 +1,60 @@
 package CodeChef;
 
-public class ShufflingParities {
+import java.util.Scanner;
 
+public class ShufflingParities {
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int t= scan.nextInt();
+        while (t-->0)
+        {
+           int N= scan.nextInt();
+           long[] A=new long[N];
+            for(int i=0;i<N;i++)
+            {
+                A[i]= scan.nextLong();
+            }
+           int es,os;
+           if(N%2==0)
+           {
+               es=N/2;
+               os=N/2;
+           }else
+           {
+               es=N/2;
+               os=N/2+1;
+           }
+
+           int ce=0,co=0;
+           for(long i:A)
+           {
+               if(i%2==0)
+               {
+                   ce++;
+               }else
+               {
+                   co++;
+               }
+           }
+
+           int sum=0;
+           if(co<=es)
+           {
+              sum+=co;
+           }else
+           {
+               sum+=Math.min(co,es);
+           }
+           if(ce<=os)
+           {
+              sum+=ce;
+           }else
+           {
+               sum+=Math.min(ce,os);
+           }
+
+            System.out.println(sum);
+        }
+    }
 }
